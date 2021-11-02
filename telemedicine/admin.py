e5b808d2351ba1bf8167f35e69646ef5af48e79e
deniseM <61604491+wassidenise@users.noreply.github.com>
2021-11-01 20:39:55 -0400

    merging copypaste agora and master-adding back the database changes and appointment stuff

--- telemedicine/admin.py
+++ telemedicine/admin.py
@@ -8,6 +8,8 @@ from .models import chartreports
 from .models import appointments
 from .models import visits
 from .models import recordings
+from .models import slots
+from .models import appointment_date
 
 admin.site.register(Post)
 admin.site.register(patients)
@@ -18,3 +20,5 @@ admin.site.register(chartreports)
 admin.site.register(appointments)
 admin.site.register(visits)
 admin.site.register(recordings)
+admin.site.register(slots)
+admin.site.register(appointment_date)
